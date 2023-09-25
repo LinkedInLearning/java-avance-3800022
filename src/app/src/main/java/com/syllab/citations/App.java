2ccdc68df0ba104082c9e0cd23b3c81049d39352
Labasse <labasse@gmail.com>
2023-09-25 13:38:29 +0000

    Analyser des expressions régulières (fin)

--- src/app/src/main/java/com/syllab/citations/App.java
+++ src/app/src/main/java/com/syllab/citations/App.java
@@ -6,19 +6,18 @@ public class App {
     public static void main(String[] args) {
         var texte = "\"Il y a 2 sortes d'amour : l'amour insatisfait, qui vous rend odieux, l'amour satisfait, qui vous rend idiot.\" Colette (1873-1954)";
 
-        // "[A-Z].*\." \w+ \([-0-9]+\)
+        // Citation : "[A-Z].*\." \w+ \([-0-9]+\)
 
-        /* Valider
+        /* Valider*/
         var citation = Pattern.compile("\"[A-Z].*\\.\" \\w+ \\([-0-9]+\\)");
         if(citation.matcher(texte).matches()) {
             System.out.println("C'est une citation.");
         }
         else {
             System.out.println("Ce texte ne semble pas être une citation.");
-        }
-        */
+        }        
 
-        /* Trouver
+        /* Trouver*/
         var nombre = Pattern.compile("\\d+");
         var occurenceNombre = nombre.matcher(texte);
         var compteur = 0;
@@ -27,9 +26,9 @@ public class App {
             compteur++;
         }
         System.out.printf("%d nombres ont été trouvés.\n", compteur);
-        */
+        
 
-        /* Extraire
+        /* Extraire*/
         var construction = Pattern.compile("l'amour (\\w+), qui vous rend (\\w+)");
         var occurence = construction.matcher(texte);
 
@@ -41,6 +40,6 @@ public class App {
                 occurence.group(2)
             );
         }
-        */
+        
     }
 }
