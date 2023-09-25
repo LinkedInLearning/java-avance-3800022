869db2d1cfc18c12fd9a6c801a7b5a43ae561392
Labasse <labasse@gmail.com>
2023-09-25 11:46:04 +0000

    Intégrer les dates et heures (fin)

--- src/app/src/main/java/com/syllab/fetes/App.java
+++ src/app/src/main/java/com/syllab/fetes/App.java
@@ -17,43 +17,49 @@ public class App {
     public static void main(String[] args) {
         var horloge = Clock.systemUTC();
         // Dates
-        var aujourdhui = LocalDate.now();
+        var aujourdhui = LocalDate.now(horloge);
         var apresDemain = aujourdhui.plusDays(2);
         var noel = LocalDate.of(2023, Month.DECEMBER, 25);
                 
         System.out.println(aujourdhui);
         System.out.println(apresDemain);
 
-        /* Formatage
-        System.out.println(noel.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT)));
-        System.out.println(noel.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM)));
-        System.out.println(noel.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG)));
+        /* Formatage */
+        System.out.println(noel.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT).withLocale(Locale.FRANCE)));
+        System.out.println(noel.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM).withLocale(Locale.FRANCE)));
+        System.out.println(noel.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL).withLocale(Locale.FRANCE)));
         System.out.println(noel.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
-        */
+        
 
-        /* Lecture
+        /* Lecture */
         var printemps = LocalDate.parse(
             "20/03/2024", 
             DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT).withLocale(Locale.FRANCE)
         );
 
         System.out.println(printemps.format(
-            DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL).withLocale(Locale.FRANCE)
+            DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL)
         ));
-        */
         
-        /* Heures
+        
+        /* Heures */
         var maintenant = LocalTime.now(horloge);
 
-        System.out.println(maintenant.format(DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT)));
+        System.out.println(maintenant.format(DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT).withLocale(Locale.FRANCE)));
         System.out.println(maintenant.format(DateTimeFormatter.ofLocalizedTime(FormatStyle.MEDIUM)));
         System.out.println(maintenant.format(DateTimeFormatter.ISO_TIME));
-        */
+        
 
-        /* Dates + heures / fuseaux
+        /* Dates + heures / fuseaux */
         var nouvelAn          = LocalDateTime.of(2024, 1, 1, 0, 0, 0, 0);
         var nouvelAnParis     = ZonedDateTime.of(nouvelAn, ZoneId.of("Europe/Paris"));
         var nouvelAnGreenwich = OffsetDateTime.of(nouvelAn, ZoneOffset.ofHours(0));
-        */
+        
+        if(nouvelAnParis.toInstant().isBefore(nouvelAnGreenwich.toInstant())) {
+            System.out.println("Paris change d'année avant le méridien 0");
+        }
+        else {
+            System.out.println("Paris change d'année après ou avec le méridien 0");
+        }
     }
 }
