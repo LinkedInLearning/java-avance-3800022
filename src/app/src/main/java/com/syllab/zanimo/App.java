5e272a5ed85e53938ffe8509e7bf9acaecb827fb
Labasse <labasse@gmail.com>
2023-09-26 14:36:57 +0000

    Communiquer avec une API Restful (fin)

--- src/app/src/main/java/com/syllab/zanimo/App.java
+++ src/app/src/main/java/com/syllab/zanimo/App.java
@@ -1,7 +1,12 @@
 package com.syllab.zanimo;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpResponse.BodyHandlers;
 
 public class App {
     public static void main(String[] args) {
@@ -13,8 +18,33 @@ public class App {
             var base = new URI("https://petstore.swagger.io");
 
             // POST /v2/pet
+            var requete = HttpRequest.newBuilder(base.resolve("/v2/pet"))
+                .headers("Content-Type", "application/json")
+                .POST(BodyPublishers.ofString(rex.toJson()))
+                .build();
+            var client = HttpClient.newHttpClient();
+            var reponse = client.send(requete, BodyHandlers.ofString());
+
+            System.out.printf("%d : %s\n", reponse.statusCode(), reponse.body());
+
+            System.out.println();
 
             // GET /v2/pet/findByStatus
+            reponse = client.send(
+                HttpRequest
+                    .newBuilder(base.resolve("/v2/pet/findByStatus?status=pending"))
+                    .GET()
+                    .build(), 
+                BodyHandlers.ofString()
+            );
+
+            System.out.printf("%d : %s", reponse.statusCode(), reponse.body());
+        }
+        catch(IOException e) {
+            e.printStackTrace(System.err);
+        }
+        catch(InterruptedException e) {
+            e.printStackTrace(System.err);
         }
         catch(URISyntaxException e) {
             e.printStackTrace(System.err);
