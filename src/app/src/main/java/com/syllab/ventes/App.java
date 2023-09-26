45551887d01b186c5b8a99d7c48136badaf384f9
Labasse <labasse@gmail.com>
2023-09-26 07:57:27 +0000

    S'initier aux flux (fin)

--- src/app/src/main/java/com/syllab/ventes/App.java
+++ src/app/src/main/java/com/syllab/ventes/App.java
@@ -1,8 +1,8 @@
 package com.syllab.ventes;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class App {
     public static void main(String[] args) {
@@ -14,17 +14,14 @@ public class App {
             catalogue.add(new Article("SK02", "Skateboard", 39.90));
             catalogue.add(new Article("CS43", "Casque"    , 19.50));
             catalogue.add(new Article("GP98", "Grip"      , 7.95));
-
-            catalogue.sort((a, b) -> a.nom().compareTo(b.nom()));
-
-            System.out.println(Arrays.toString(catalogue.toArray()));
-
+            catalogue.stream()
+                .filter(a -> !a.estAbandonne())
+                .sorted((a, b) -> a.nom().compareTo(b.nom()))
+                .map(a -> String.format("[%s] %s", a.ref(), a.nom()))
+                .forEach(System.out::println);
+            
             // Recherche par référence CS43
-            var articlesParRef = new HashMap<String, Article>();
-        
-            for(var article : catalogue) {
-                articlesParRef.put(article.ref(), article);
-            }          
+            var articlesParRef = catalogue.stream().collect(Collectors.toMap(a->a.ref(), Function.identity()));
 
             var rechercheRef = "CS43";
 
