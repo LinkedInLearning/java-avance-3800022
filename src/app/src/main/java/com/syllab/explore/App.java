555360662f6f51654413e29d742c5ab9afc637be
Labasse <labasse@gmail.com>
2023-09-26 13:00:02 +0000

    Combiner les flux d'entrées/sorties (fin)

--- src/app/src/main/java/com/syllab/explore/App.java
+++ src/app/src/main/java/com/syllab/explore/App.java
@@ -3,30 +3,35 @@ package com.syllab.explore;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
+import java.io.BufferedInputStream;
 import java.io.IOException;
 
 public class App {
     public static void main(String[] args) {
-        try(var contenu = Files.newDirectoryStream(Path.of("."))) {
-            var stats = new StringBuilder();
-
+        try(var contenu = Files.newDirectoryStream(Path.of("."));
+            var fluxSortie = Files.newBufferedWriter(Path.of("./stats.txt"), StandardOpenOption.CREATE)) {
+            
             contenu.forEach(p -> {
                 if(!Files.isDirectory(p)) {
-                    stats.append(p.getFileName());
                     try {
-                        stats.append(lireFichierTexte(p));  
-                    }
-                    catch (IOException e) {
+                        fluxSortie.write(p.getFileName().toString());
                         try {
-                            stats.append(lireFichierBinaire(p));
+                            fluxSortie.write(lireFichierTexte(p));  
                         }
-                        catch(IOException eBin) {
-                            stats.append(String.format(" : %s.\n", eBin.getMessage()));
+                        catch (IOException e) {
+                            try {
+                                fluxSortie.write(lireFichierBinaire(p));
+                            }
+                            catch(IOException eBin) {
+                                fluxSortie.write(String.format(" : %s.\n", eBin.getMessage()));
+                            }
                         }
+                    }
+                    catch(IOException e) {
+                        System.err.printf("Impossible d'écrire la ligne pour %s.", p);
                     }
                 }
             });
-            Files.writeString(Path.of("./stats.txt"), stats.toString(), StandardOpenOption.CREATE);
         }
         catch(IOException e) {
             System.err.print("Impossible de parcourir le répertoire courant : ");
@@ -34,24 +39,30 @@ public class App {
         }
     }
     public static String lireFichierTexte(Path p) throws IOException {
-        var lines = Files.readAllLines(p);
-
-        return String.format(
-            " : %d lignes dont %d contiennent le mot 'java'.\n", 
-            lines.size(),
-            lines.stream()
-                .filter(l -> l.toLowerCase().contains("java"))
-                .count()
-        );
+        try(var fluxTexte = Files.newBufferedReader(p)) {
+            int nlignes, njava;
+            String line;
+        
+            for(nlignes = njava = 0; (null != (line = fluxTexte.readLine())); nlignes++) {
+                if(line.toLowerCase().contains("java")) {
+                    njava++;
+                }
+            }
+            return String.format(" : %d lignes dont %d contiennent le mot 'java'.\n", nlignes, njava);
+        }
     }
     public static String lireFichierBinaire(Path p) throws IOException {
-        var octets = Files.readAllBytes(p);
+        try(var fluxBinaire = Files.newInputStream(p); 
+            var buf = new BufferedInputStream(fluxBinaire, 13))
+        {
+            var octets = buf.readNBytes(13);
 
-        if(octets[0]==(byte)0xff && octets[1]==(byte)0xd8) {
-            return String.format(" : Fichier JPEG version %d.%d\n", octets[11], octets[12]);
-        }
-        else {
-            return "Format inconnu.\n";
+            if(octets[0]==(byte)0xff && octets[1]==(byte)0xd8) {
+                return String.format(" : Fichier JPEG version %d.%d\n", octets[11], octets[12]);
+            }
+            else {
+                return "Format inconnu.\n";
+            }
         }
     }
 }
