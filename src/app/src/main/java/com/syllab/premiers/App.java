25ca82ecaf561b55f781eb4d75a7222a51999bdd
Labasse <labasse@gmail.com>
2023-09-27 07:44:11 +0000

    Organiser l'exécution (fin)

--- src/app/src/main/java/com/syllab/premiers/App.java
+++ src/app/src/main/java/com/syllab/premiers/App.java
@@ -1,8 +1,11 @@
 package com.syllab.premiers;
 
+import java.util.concurrent.ForkJoinPool;
+
 public class App {
     public static void main(String[] args) {
-        var premiers = new RecherchePremiers(1, 200_000).trouver();
+        var pool = ForkJoinPool.commonPool();
+        var premiers = pool.invoke(new RecherchePremiers(1, 200_000));
 
         premiers.stream().limit(500).forEach(n -> System.out.printf("%d ", n));
         System.out.printf("%d nombres premiers trouvés\n", premiers.size());
--- src/app/src/main/java/com/syllab/premiers/RecherchePremiers.java
+++ src/app/src/main/java/com/syllab/premiers/RecherchePremiers.java
@@ -2,27 +2,16 @@ package com.syllab.premiers;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.RecursiveTask;
 
-public class RecherchePremiers {
+public class RecherchePremiers extends RecursiveTask<List<Long>> {
     private final long debut, fin;
 
     public RecherchePremiers(long debut, long fin) {
         this.debut = debut;
         this.fin = fin;
     }
-    
-    protected List<Long> trouver() {
-        List<Long> resultats;
-
-        resultats = new ArrayList<Long>();
-        for(long i = this.debut; i<this.fin; i++) {
-            if(estPremier(i)) {
-                resultats.add(i);
-            }
-        }
-        return resultats;
-    }
-
+  
     private boolean estPremier(long n) {
         for(long i=2; i<n; i++) {
             if(n%i == 0) {
@@ -30,5 +19,31 @@ public class RecherchePremiers {
             }
         }
         return true;
+    }
+
+    private final static long SEUIL = 200;
+
+    @Override
+    protected List<Long> compute() {
+        List<Long> resultats;
+
+        if(this.fin - this.debut < SEUIL) {
+            resultats = new ArrayList<Long>();
+            for(long i = this.debut; i<this.fin; i++) {
+                if(estPremier(i)) {
+                    resultats.add(i);
+                }
+            }
+        }
+        else {
+            var milieu = (this.debut+this.fin)/2;
+            var moitie1 = new RecherchePremiers(this.debut, milieu);
+            var moitie2 = new RecherchePremiers(milieu, this.fin);
+
+            moitie2.fork();
+            resultats = moitie1.compute();
+            resultats.addAll(moitie2.join());
+        }
+        return resultats;
     }
 }
